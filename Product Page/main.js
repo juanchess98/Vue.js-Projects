c00484222764f17324feca53db4ef377ecc55cab
juanchess98 <juanchess98@gmail.com>
2022-01-24 15:36:33 -0500

    Vue App mounted

---
+++ Product Page/main.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            description: 'This is the decription of these comfortable socks'
+
+        }
+    }
+})
+
