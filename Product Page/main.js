a0d73a71085c7c94f5989b1e89bddad3431947e3
juanchess98 <juanchess98@gmail.com>
2022-01-24 16:09:59 -0500

    attribute binding for image and an anchor tag

--- Product Page/main.js
+++ Product Page/main.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            description: 'This is the decription of these comfortable socks'
+            description: 'This is the decription of these comfortable socks',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://vuejs.org/'
 
         }
     }
