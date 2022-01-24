2af8cd14c906f64c70d231a0d33b5d539b2e2872
juanchess98 <juanchess98@gmail.com>
2022-01-24 18:50:50 -0500

    Event Handling, adding som events to change the image color of the sockets and events to add or substract the value cart

--- Product Page/main.js
+++ Product Page/main.js
@@ -1,17 +1,31 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart:0,
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
             inventory: 0,
             details: ['50% Cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green'},
-                { id: 2235, color: 'blue'},
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
+                { id: 2235, color: 'blue', image:'./assets/images/socks_blue.jpg'},
 
             ],
-            sizes: ['small', 'medium', 'large']
+        }
+    },
+    methods: {
+        addToCart(){
+            this.cart += 1
+        },
+        updateImage(variantImage){
+            this.image = variantImage
+        },
+        subtractToCart() {
+            if (this.cart > 0){
+                this.cart -= 1
+            }
         }
     }
-})
+}
+)
 
