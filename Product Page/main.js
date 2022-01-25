344a93a11483ab248983297511022e730423d828
juanchess98 <juanchess98@gmail.com>
2022-01-25 09:04:28 -0500

    Comunicating Events

--- Product Page/main.js
+++ Product Page/main.js
@@ -1,28 +1,18 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart:0,
-            product: 'Socks',
-            image: './assets/images/socks_green.jpg',
-            inventory: 0,
-            details: ['50% Cotton', '30% wool', '20% polyester'],
-            variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
-                { id: 2235, color: 'blue', image:'./assets/images/socks_blue.jpg'},
-
-            ],
+            cart:[],
+            premium: false
         }
     },
     methods: {
-        addToCart(){
-            this.cart += 1
+        updateCart(id) {
+            this.cart.push(id)
         },
-        updateImage(variantImage){
-            this.image = variantImage
-        },
-        subtractToCart() {
-            if (this.cart > 0){
-                this.cart -= 1
+        removeById(id) {
+            const index = this.cart.indexOf(id)
+            if (index > -1) {
+                this.cart.splice(index, 1)
             }
         }
     }
